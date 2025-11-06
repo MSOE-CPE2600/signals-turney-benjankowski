bbe4885f59978343f5de7201761a1f52af7105a7
jkowskib <jkowskib@msoe.edu>
2025-11-05 21:31:47 -0600

    Part 3 complete

--- signal_alarm.c
+++ signal_alarm.c
@@ -1,6 +1,29 @@
 /**
- * File: 
- * Modified by:
+ * File: signal_alarm.c
+ * Modified by: Benjamin Jankowski
  * 
  * Brief summary of program:
+ * Creates and then captures a SIGALRM signal
  */
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+void handle_alarm() {
+    printf("Alarm was raised\n");
+}
+
+int main(void) {
+    signal(SIGALRM, handle_alarm);
+
+    // https://linux.die.net/man/3/alarm
+    alarm(5); // Schedule an SIGALRM for 5 seconds from now
+
+    for (int i = 0; i < 10; i++) {
+        // The sleep will get canceled by the alarm
+        // The loop is used to break up the sleeps instead
+        sleep(1);
+    }
+
+    return 0;
+}
--- signal_segfault.c
+++ signal_segfault.c
@@ -4,15 +4,24 @@
  */
 
 /**
- * Modified by:
+ * Modified by: Benjamin Jankowski
  * 
  * Brief summary of modifications:
+ * Added `handle_segfault` method with signal registration to capture segmentation fault events
  */
-
-
+#include <signal.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+void handle_segfault() {
+    printf("We had a segmentation fault!\n");
+    exit(1);
+}
 
 int main (int argc, char* argv[]) {
+    signal(SIGSEGV, handle_segfault);
+
     // Declare a null pointer
     int* i = NULL;
 
